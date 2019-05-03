15ddec917392a1126e72277ce6a80677f9b3a581
Charles Harvey <harvey.charles@gmail.com>
2019-05-03 09:47:46 +0200

    add header and footer code

--- footer.php
+++ footer.php
@@ -1,52 +1,57 @@
 <!-- footer -->
 <?php $tdu = get_template_directory_uri(); ?>
-<footer class="footer" role="contentinfo">
-  <div class="footer_top">
+
+
+<footer role="contentinfo">
+
     <div class="container">
-      <div class="row">
-        <div class="col-sm-4">
-          <h3>Contactez-nous</h3>
-          <p>Voyages d’affaires  <br>
-            +41 (0)22 929 8 929 <br>
-            <a href="mailto:business@e-travel.ch">business@e-travel.ch</a>
-          </p>
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Liens utiles</h3>
+                <ul>
 
-          <p>Voyages d’agrément <br>
-            +41 (0)22 707 73 10 <br>
-            <a href="mailto:tourisme@e-travel.ch">tourisme@e-travel.ch</a>
-          </p>
-          <a target="_blank" href="
-          https://www.facebook.com/executivetravelsa/?__tn__=%2Cd%2CP-R&eid=ARBxt-MXwp6WQzXa20F1xBpmDbwQs0rktKA252qvw22xBOJZmdRrAN7UdpuUT3vHZlyW-GxAi4uKZctj" class="social_icon fb_footer">Facebook</a>
-          <a target="_blank"  href="https://www.instagram.com/executive_travel_geneve" class="social_icon insta_footer">Instagram</a>
-          <div class="fb-like" data-href="https://www.facebook.com/executivetravelsa/" data-layout="button" data-action="like" data-size="small" data-show-faces="false" data-share="true" style="position: relative; top: 14px;"></div>
+                    <li><a href="#">INSCRIPTIONS</a></li>
+                    <li><a href="#">Enseignement</a></li>
+                    <li><a href="#">Internat</a></li>
+                    <li><a href="#">Inscription activités</a></li>
+                    <li><a href="#">Camps été - hiver</a></li>
+
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <h3>Nous trouver</h3>
+                <p>
+                    Ecole Nouvelle de la Suisse Romande <br>
+                    Ch. de Rovéréaz 20  <br>
+                    C.P. 161 CH – 1000 Lausanne 12   <br>
+                    Suisse
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Nous contacter</h3>
+                <p>
+                    <strong>Tel :</strong> +41 21 654 65 00 <br>
+                    <strong>Fax :</strong> +41 21 654 65 05 <br>
+                    <strong>Email :</strong>  info(at)ensr.ch
+                </p>
+            </div>
         </div>
-        <div class="col-sm-8">
-          <h3>Newsletter</h3>
-          <p>Pour être tenu au courant de nos promotions et actualités vous pouvez vous inscrire à notre newsletter ici </p>
-          <?php $form_widget = new \MailPoet\Form\Widget();
-            echo $form_widget->widget(array('form' => 1, 'form_type' => 'php'));
-          ?>
+    </div><!--  END OF .container -->
 
-          <div id="garantie_suisse">
-              <img src="<?php echo $tdu; ?>/img/logo_garantie_suisse.jpg" alt="logo fond de garantie suisse" />
-          </div>
 
+
+
+    <div id="copyright_notice">
+        <div class="container">
+            <p>&copy; Ecole Nouvelle de la Suisse Romande |  <a href="https://webfactor.ch" target="_blank">Website by Webfactor</a></p>
         </div>
-      </div>
     </div>
 
-  </div>
-
-  <div class="footer_bottom">
-    <p class="container">
-      &copy; <?php echo date('Y'); ?> Copyright ©<?php bloginfo('name'); ?> | Website by <a href="//webfactor.ch" title="Webfactor">Webfactor</a>
-    </p>
-  </div>
-
+    <div class="footer_background"></div>
 </footer>
-<!-- /footer -->
 
-</div><!-- END OF rest_of_page -->
+</main>
+
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -5,14 +5,7 @@
     <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
         <?php $tdu = get_template_directory_uri(); ?>
         <link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tdu; ?>/img/bullet.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tdu; ?>/img/bullet.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tdu; ?>/img/bullet.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="<?php echo $tdu; ?>/img/favicon/safari-pinned-tab.svg" color="#00476e">
-        <meta name="msapplication-TileColor" content="#5bc0d5">
-        <meta name="theme-color" content="#5bc0d5">
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:500,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Spectral:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php bloginfo('description'); ?>">
@@ -21,34 +14,30 @@
     </head>
     <body <?php body_class(); ?>>
 
-        <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&autoLogAppEvents=1&version=v3.2&appId=2220617461487737';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
 
 
-
-        <header class="header" id="header">
-            <!-- <div class="container"></div> -->
-            <a href="<?php echo home_url(); ?>" class="branding"><?php bloginfo('name'); ?></a>
-            <div id="menu_items">
-            <a href="mailto:info@e-travel.ch" id="email_button" >Email</a>
-            <a href="#" id="phone_button" >Phone</a>
-            <a href="#" id="menu_button" >Menu</a>
-            </div>
-            <div id="phone_header">
-              <p>voyages d'affaires : +41 (0)22 929 8 929<br>tourisme : +41 (0)22 707 73 10</p>
-            </div>
-        </header>
-
-        <nav id="navigation_menu" role="navigation">
-          <ul>
-            <?php chilly_nav('primary-navigation'); ?>
+    <div id="top_nav">
+        <div class="container">
+            <ul>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Inscription</a></li>
             </ul>
-        </nav>
+        </div>
+    </div>
 
-    <div id="rest_of_page">
+    <header>
+
+        <div class="container">
+            <nav>
+
+                <a href="<?php echo home_url(); ?>" id="branding">ENSR</a>
+                <ul>
+                    <?php chilly_nav('primary-navigation'); ?>
+                </ul>
+            </nav>
+        </div>
+        <a href="#" id="menu_button">Menu</a>
+    </header>
+
+
+    <main id="main">
