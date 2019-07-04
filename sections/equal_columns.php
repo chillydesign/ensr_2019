db319e00867c58673457745af5ac98c990bd3fc4
Charles Harvey <harvey.charles@gmail.com>
2019-07-04 20:02:30 +0100

    add revers col classes

--- sections/equal_columns.php
+++ sections/equal_columns.php
@@ -1,18 +1,24 @@
 <?php $column_count =  sizeof(  get_sub_field('column')  ); ?>
 <?php $column_class = count_to_bootstrap_class($column_count); ?>
+<?php $reverse_on_mobile =  get_sub_field('reverse_order_on_mobile'); ?>
+<?php
 
-
+$rev_class = array('', '');
+if ($reverse_on_mobile) :
+	$rev_class = array('col-sm-push-6', 'col-sm-pull-6');
+ endif;
+ ?>
 
 <div class="colonnes_padding">
 <div class="container">
 	<div class="row">
-	<?php while ( have_rows('column') ) : the_row(); ?>
-		<div class="<?php echo $column_class; ?> ">
+	<?php $i = 0; while ( have_rows('column') ) : the_row(); ?>
+		<div class="<?php echo $column_class   . ' ' . $rev_class[$i % $column_count] ; ?> ">
 			<div class="column">
 				<?php echo get_sub_field('column_content'); ?>
 			</div>
 		</div>
-	<?php endwhile; ?>
+	<?php $i++; endwhile; ?>
 	</div> <!-- END OF ROW -->
 </div><!--  END OF CONTAINER -->
 </div>
