f7fc14dcd343dc876566a01b1c9598bfe010d0de
Charles Harvey <harvey.charles@gmail.com>
2019-07-29 17:21:06 +0200

    make two thirds section col-sm-8

--- sections/two_thirds_one_third.php
+++ sections/two_thirds_one_third.php
@@ -1,17 +1,16 @@
-<?php $small_col_position = get_sub_field('small_col_position'); ?> 
-<?php $classes = ($small_col_position == 'right') ?  [ 'col-sm-9 col-sm-text-right', 'col-sm-3' ]  :  [ 'col-sm-9 col-sm-push-3', 'col-sm-3 col-sm-pull-9' ]  ; ?> 
+<?php $small_col_position = get_sub_field('small_col_position'); ?>
+<?php $classes = ($small_col_position == 'right') ?  [ 'col-sm-8 col-sm-text-right', 'col-sm-4' ]  :  [ 'col-sm-8 col-sm-push-4', 'col-sm-4 col-sm-pull-8' ]  ; ?>
 <div class="container">
 	<div class="row">
 
-
 		<div class="<?php echo $classes[0]; ?>">
-		<?php echo get_sub_field('two_thirds_content'); ?>
+			<?php echo get_sub_field('two_thirds_content'); ?>
 		</div>
-
 
 		<div class="<?php echo $classes[1]; ?>">
 			<?php echo get_sub_field('one_third_content'); ?>
 		</div>
 
-	</div> <!-- END OF ROW -->
-    </div>
+	</div>
+	<!-- END OF ROW -->
+</div>
