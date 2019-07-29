4bb3c2de38148bdfbc4c7aaaab1e8f12d1d64e6b
Charles Harvey <harvey.charles@gmail.com>
2019-07-29 17:40:21 +0200

    styled th

--- sections/slider.php
+++ sections/slider.php
@@ -1,7 +1,7 @@
 
 
 <div class="slick_slider_no_dots">
-<?php while ( have_rows('slides') ) : the_row() ; ?>
+<?php while (have_rows('slides')) : the_row() ; ?>
 
   <?php $image =  get_sub_field('image'); ?>
   <?php $slide_content =  get_sub_field('slide_content'); ?>
@@ -9,7 +9,7 @@
   <div class="slide">
       <div class="container">
           <div class="row">
-              <div class="col-sm-6 ">
+              <div class="col-sm-6 col-sm-push-6">
 
                       <div class="welcome_text_inner">
                           <?php echo $slide_content; ?>
