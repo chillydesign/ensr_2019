561a41c5625c089a45818e83a7fe9f7122bfddb4
Melissa Rissel <rissel.melissa@gmail.com>
2019-08-14 11:10:21 +0200

    qwe

--- template-landing.php
+++ template-landing.php
@@ -43,10 +43,12 @@
                 <div class="background_image" style="background-image:url('<?php echo $tdu;  ?>/images/buildings_1.jpg')"></div>
               <?php endif; ?>
 
-              <div class="container" style="position: absolute; top: 100px; width: 100%;">
+              <div style="position: absolute; top: 100px; width: 100%;">
+                <div class="container">
                 <div class="row">
                 <div class="col-sm-6 col-sm-push-6">
                   <div class="landingform"><?php the_field('form'); ?></div>
+                </div>
                 </div>
                 </div>
 
