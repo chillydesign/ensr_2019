c0f7db382b37ee1533ec2d08b19ff2de7185fdce
Melissa Rissel <rissel.melissa@gmail.com>
2019-09-23 07:01:00 +0100

    we

--- template-home.php
+++ template-home.php
@@ -89,7 +89,7 @@
                         <div class="school_areas_container">
                           <!-- <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/60137322?color=000000&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script> -->
                           <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe id="ytplayer" type="text/html"
-src="https://www.youtube.com/embed/M7lc1UVf-VE?loop=1&modestbranding=1"
+src="https://www.youtube.com/embed/bmQYywa3JAM?loop=1&modestbranding=1"
 frameborder="0" allowfullscreen>  </div>
 
 
