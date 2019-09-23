a5d3a166629eaf930bbcb62708789fe516c489e5
Melissa Rissel <rissel.melissa@gmail.com>
2019-09-23 09:05:36 +0100

    qwer

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ External Modules/Files
 Theme Support
 \*------------------------------------*/
 
-// setlocale(LC_TIME, "fr_FR"); //only necessary if the locale isn't already set
+setlocale(LC_TIME, "fr_FR"); //only necessary if the locale isn't already set
 
 
 if (!isset($content_width)) {
