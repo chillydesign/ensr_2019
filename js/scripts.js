62734ee4b1546d58c88f57ce2a9ab07f42063cce
Melissa Rissel <rissel.melissa@gmail.com>
2019-10-07 23:35:52 +0200

    djeprp

--- js/scripts.js
+++ js/scripts.js
@@ -119,8 +119,7 @@
         $home_page_message.hide();
 
       var d = new Date();
-      // d.setTime(d.getTime() + (60*60*1000));
-      d.setTime(d.getTime() + (1));
+      d.setTime(d.getTime() + (60*60*1000));
       var expires = "expires="+ d.toUTCString();
       document.cookie = "home_page_message_hide=true;"+ expires +"; path=/";
 
