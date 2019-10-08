44799500c2493f13ca251524402597d79354eb26
Melissa Rissel <rissel.melissa@gmail.com>
2019-10-08 11:27:18 +0200

    map in footer

--- social_links.php
+++ social_links.php
@@ -1,4 +1,5 @@
 <ul class="social_icons">
     <li class="social_icon icon_facebook"><a target="_blank" href="https://www.facebook.com/EcoleNouvelleENSR/">Facebook</a></li>
     <li class="social_icon icon_instagram"><a target="_blank" href="https://www.instagram.com/explore/locations/497566922/ensr/">Instagram</a></li>
+      <li class="social_icon icon_map"><a target="_blank" href="https://www.instagram.com/explore/locations/497566922/ensr/">Google Map</a></li>
 </ul>
