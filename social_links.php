f6141a19f5241795e6cd5ac9d450c1b757e5ed04
Melissa Rissel <rissel.melissa@gmail.com>
2019-10-08 11:33:47 +0200

    directions in footer

--- social_links.php
+++ social_links.php
@@ -1,5 +1,5 @@
 <ul class="social_icons">
     <li class="social_icon icon_facebook"><a target="_blank" href="https://www.facebook.com/EcoleNouvelleENSR/">Facebook</a></li>
     <li class="social_icon icon_instagram"><a target="_blank" href="https://www.instagram.com/explore/locations/497566922/ensr/">Instagram</a></li>
-      <li class="social_icon icon_map"><a target="_blank" href="https://www.instagram.com/explore/locations/497566922/ensr/">Google Map</a></li>
+      <li class="social_icon icon_map"><a target="_blank" href="https://www.google.co.uk/maps/dir//%C3%89cole+Nouvelle+de+la+Suisse+Romande,+Chemin+de+Rov%C3%A9r%C3%A9az+20,+1012+Lausanne,+Switzerland/@46.5268424,6.6526495,17z/data=!4m16!1m6!3m5!1s0x478c2e451624bee5:0x7316237fe5875dcb!2s%C3%89cole+Nouvelle+de+la+Suisse+Romande!8m2!3d46.5268387!4d6.6548382!4m8!1m0!1m5!1m1!1s0x478c2e451624bee5:0x7316237fe5875dcb!2m2!1d6.6548382!2d46.5268387!3e3">Google Map</a></li>
 </ul>
