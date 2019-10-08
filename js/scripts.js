f084b40ddfcdb61891ad93c2f690d9989c4cd8ac
Charles Harvey <harvey.charles@gmail.com>
2019-10-08 06:25:05 +0100

    remove featherligh js from scripts

--- js/scripts.js
+++ js/scripts.js
@@ -111,18 +111,7 @@
     // SLIDER
 
 
-    // GALLERY
-    // GALLERY
 
-    $('a.gallery').featherlightGallery({
-      previousIcon: '&#9664;',     /* Code that is used as previous icon */
-      nextIcon: '&#9654;',         /* Code that is used as next icon */
-      galleryFadeIn: 100,          /* fadeIn speed when slide is loaded */
-      galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
-    });
-
-    // GALLERY
-    // GALLERY
 
 
 
