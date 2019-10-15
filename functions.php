e6f7ade1b745a156600a7891069024777cf0b4e9
Melissa Rissel <rissel.melissa@gmail.com>
2019-10-15 16:49:42 +0200

    wedf

--- functions.php
+++ functions.php
@@ -677,7 +677,7 @@ function translateString($string)
                 return "search";
             } elseif ($string == "Pas encore d'évènements") {
                 return "No events yet";
-            } elseif ($string == "Découvrez notre Programme en anglais BI à partir de l'école primaire >>") {
+            } elseif ($string == "Découvrez notre Programme en anglais à partir de l'école primaire >>") {
                 return "Discover our English programme starting from primary school >>";
             } elseif ($string == "Préférez-vous que la langue principale d'enseignement de votre enfant soit l'anglais?") {
                 return "Would you prefer your child to be taught primarily in English?";
