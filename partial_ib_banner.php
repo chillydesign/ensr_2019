d19464a08e270a3e6ae06ac14d8fd96ad43d1378
Melissa Rissel <rissel.melissa@gmail.com>
2019-12-08 16:59:36 +0100

    cookie banner

--- partial_ib_banner.php
+++ partial_ib_banner.php
@@ -12,4 +12,14 @@
 <a href="#" id="cancel_ib_banner">+</a>
 <div class="shield_background"></div>
 </div>
+<div class="cookie_policy">
+  <div class="container">
+    <?php if (site_language() == 'en') : ?>
+      By using this website you are agreeing to our <a target="
+      " href="https://ensr.ch/cookie-policy/">cookie policy</a>
+    <?php else : ?>
+      En utilisant ce site vous acceptez notre <a target="
+      " href="https://ensr.ch/fr/politique-dutilisation-des-cookies/">politique d'utilisation des cookies</a>
+  </div>
+</div>
 </div>
