550da1812df53f79358a338c8938ff8d25e1474f
Charles Harvey <harvey.charles@gmail.com>
2020-02-14 16:15:43 +0100

    add separate images to sectin twothirdsonethird

--- sections/two_thirds_one_third.php
+++ sections/two_thirds_one_third.php
@@ -1,14 +1,35 @@
-<?php $small_col_position = get_sub_field('small_col_position'); ?>
-<?php $classes = ($small_col_position == 'right') ?  [ 'col-sm-8 col-sm-text-right', 'col-sm-4' ]  :  [ 'col-sm-8 col-sm-push-4', 'col-sm-4 col-sm-pull-8' ]  ; ?>
+<?php
+
+$small_col_position = get_sub_field('small_col_position'); 
+$use_two_thirds_image = get_sub_field('use_two_thirds_image'); 
+$use_one_third_image = get_sub_field('use_one_third_image'); 
+
+
+if( $use_one_third_image ) {
+	$one_third_content =  get_sub_field('one_third_image');
+	var_dump($one_third_content); 
+} else {
+	$one_third_content =  get_sub_field('one_third_content');
+}
+
+if( $use_two_thirds_image ) {
+	$two_thirds_content =  get_sub_field('two_thirds_image');
+} else {
+	$two_thirds_content =  get_sub_field('two_thirds_content');
+}
+
+$classes = ($small_col_position == 'right') ?  [ 'col-sm-8 col-sm-text-right', 'col-sm-4' ]  :  [ 'col-sm-8 col-sm-push-4', 'col-sm-4 col-sm-pull-8' ]  ;
+
+?>
 <div class="container">
 	<div class="row">
 
 		<div class="<?php echo $classes[0]; ?>">
-			<?php echo get_sub_field('two_thirds_content'); ?>
+			<?php echo $two_thirds_content; ?>
 		</div>
 
 		<div class="<?php echo $classes[1]; ?>">
-			<?php echo get_sub_field('one_third_content'); ?>
+			<?php echo $one_third_content; ?>
 		</div>
 
 	</div>
